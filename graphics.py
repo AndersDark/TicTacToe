4003e301cab9da2302d6fffaff63cc7517f8d88c
Anders Mørk <anders.hmork@gmail.com>
2022-01-29 17:39:52 +0100

    First commit

---
+++ graphics.py
@@ -0,0 +1,27 @@
+
+
+import tkinter as tk
+
+TOP_BAR = 50
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = SCREEN_WIDTH + TOP_BAR
+LINE_THICKNES = 5
+BOX_SIZE = (SCREEN_WIDTH-2*LINE_THICKNES)//3
+
+root = tk.Tk();
+
+def screenSetup():
+    root.geometry(str(SCREEN_WIDTH)+"x"+str(SCREEN_HEIGHT))
+    root.resizable(False,False)
+    
+    canvas = tk.Canvas(root,width=SCREEN_WIDTH,height=SCREEN_HEIGHT,bg="white")
+    canvas.pack()
+
+    canvas.create_rectangle(BOX_SIZE,TOP_BAR,BOX_SIZE+LINE_THICKNES,SCREEN_HEIGHT,fill='black')
+    canvas.create_rectangle(2*BOX_SIZE+LINE_THICKNES,TOP_BAR,2*BOX_SIZE+2*LINE_THICKNES,SCREEN_HEIGHT,fill='black')
+    canvas.create_rectangle(0,TOP_BAR+BOX_SIZE,SCREEN_WIDTH,TOP_BAR+BOX_SIZE+LINE_THICKNES,fill='black')
+    canvas.create_rectangle(0,TOP_BAR+2*BOX_SIZE+LINE_THICKNES,SCREEN_WIDTH,TOP_BAR+2*BOX_SIZE+2*LINE_THICKNES,fill='black')
+
+    for x in range(3):
+        for y in range(3):
+            tk.Button(root,bg="white",border=False,activebackground="white",command=root.destroy).place(height=BOX_SIZE,width=BOX_SIZE,x=x*BOX_SIZE+x*LINE_THICKNES,y=TOP_BAR+y*BOX_SIZE+y*LINE_THICKNES)
---
+++ main.py
@@ -0,0 +1,11 @@
+import graphics as g
+
+
+
+def main():
+    g.screenSetup()
+    g.root.mainloop()
+    
+
+if __name__ == "__main__":
+    main()
